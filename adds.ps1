877763ec966bd02f425aecc4f4507735a3dd34c5
ZUV68954 <zuv68954@educastur.es>
2023-05-08 18:16:02 +0200

    Inicios

---
+++ adds.ps1
@@ -0,0 +1,16 @@
+$dominio = Read-Host 'Nombre de dominio: '
+
+Import-Module ADDSDeployment
+Install-ADDSDomainController `
+-NoGlobalCatalog:$false `
+-CreateDnsDelegation:$false `
+-Credential (Get-Credential) `
+-CriticalReplicationOnly:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainName "$dominio" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SiteName "Default-First-Site-Name" `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
