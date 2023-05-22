4a649825ff07965ba97d58dfce4b0a65f22d40ed
ZUV68954 <zuv68954@educastur.es>
2023-05-22 19:30:46 +0200

    modified:   Promover a Controlador/parte1.ps1 	modified:   README.md

--- Promover a Controlador/parte1.ps1
+++ Promover a Controlador/parte1.ps1
@@ -1,7 +1,7 @@
 #
 ## Gestión de errores
 #
-
+#Ayuda me han quitado el cable de red.
 $error.clear()
 $ErrorActionPreference = "Stop"
 
@@ -26,7 +26,6 @@ Start-Sleep -Seconds 3
 $hostname = HOSTNAME.EXE
 $dominio = Read-Host "Introduzca el nombre de dominio"
 $nombre = Read-Host "Introduzca un nuevo nombre para el servidor"
-$nombre = Read-Host "Introduzca las credenciales de dominio"
 Start-Sleep -Seconds 1
 try {
 Add-Computer -ComputerName $hostname -DomainName $dominio -NewName $nombre -Credential $dominio\Administrator -Restart
